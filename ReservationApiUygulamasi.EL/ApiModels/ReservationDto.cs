5a329ce658055e0f957ea20f4ad4cbcb75def216
AliHasanov16 <ayxanazevov@gmail.com>
2026-03-25 14:47:28 +0300

    Eski WebApi Silindi Yenisi Eklendi ve ApiModels Sınıfları Oluşturuldu Paketler İndirildi

---
+++ ReservationApiUygulamasi.EL/ApiModels/ReservationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationApiUygulamasi.EL.ApiModels
+{
+	public class ReservationDto
+	{
+		public int ProductRef { get; set; }
+		public double ReservedQty { get; set; }
+		public string Notes { get; set; }
+	}
+}
